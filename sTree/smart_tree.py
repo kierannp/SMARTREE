dc1033f6f6d8e45983d4a3a925c54c73fa41ed88
kierannp <nehilkieran@gmail.com>
2023-10-27 14:42:04 -0500

    Added function documentation

--- sTree/smart_tree.py
+++ sTree/smart_tree.py
@@ -6,6 +6,18 @@ import typing
 def gen_smarts(node : mb.compound.Compound, 
                G : nx.Graph, depth: int, 
                node_trees: typing.Dict[mb.compound.Compound, nx.classes.digraph.DiGraph]):
+    """
+    The dynamic programming algorithm to generate SMARTS string for a node in a graph.
+
+    Parameters
+    ----------
+    node : mb.compound.Compound
+        The node to generate the SMARTS string for.
+    G : nx.Graph
+        The graph to generate the SMARTS string for.
+    depth : int
+        The depth to traverse in G from node using a breadth first search.
+    """
     if depth == 1:
         smarts = '[{};X{}]'.format(node.element.symbol, len(list(G.neighbors(node))))
         for neigh in G.neighbors(node):
@@ -17,6 +29,16 @@ def gen_smarts(node : mb.compound.Compound,
     return smarts
 
 def bond_graph_to_smarts_dic(BG, depth=1):
+    """
+    Generates a dictionary of SMARTS strings for each node in a bond graph.
+    
+    Parameters
+    ----------
+    BG : nx.Graph
+        The bond graph to generate the SMARTS strings for.
+    depth : int
+        The depth to traverse in BG from node using a breadth first search.
+    """
     G = nx.Graph(BG._adj)
     smarts_dic = {}
     node_to_tree = {n:nx.bfs_tree(G, n) for n in G.nodes}
